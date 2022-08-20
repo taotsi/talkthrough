6879624b67b4f53563c20da9598798f001f94bad
taotsi <taotsi@protonmail.com>
2022-08-21 06:11:47 +0800

    add menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import RichTextExample from './editor';
+import {Menu, Input, Button} from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <RichTextExample/>
-    </div>
-  );
+    return (
+        <div>
+            <Menu borderless fixed={'top'}>
+                <Menu.Item>TODO：标志</Menu.Item>
+                <Menu.Item position='left'>
+                    <Input className='icon' icon='search' placeholder='TODO'/>
+                </Menu.Item>
+                <Menu.Item>
+                    <Button>Log in</Button>
+                </Menu.Item>
+                <Menu.Item>
+                    <Button>Sign up</Button>
+                </Menu.Item>
+            </Menu>
+            <RichTextExample/>
+        </div>
+    );
 }
 
 export default App;
