04f3013c334a108c9e56e94c5ff66c53fdd17da8
taotsi <taotsi@protonmail.com>
2022-08-21 16:59:23 +0800

    fix menu overlapping content by making it sticky

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import RichTextExample from './editor';
-import {Menu, Input, Button} from 'semantic-ui-react';
+import {Menu, Input, Button, Sticky, Segment, Container} from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
 function App() {
     return (
         <div>
-            <Menu borderless fixed={'top'}>
-                <Menu.Item>TODO：标志</Menu.Item>
-                <Menu.Item position='left'>
-                    <Input className='icon' icon='search' placeholder='TODO'/>
-                </Menu.Item>
-                <Menu.Item>
-                    <Button>Log in</Button>
-                </Menu.Item>
-                <Menu.Item>
-                    <Button>Sign up</Button>
-                </Menu.Item>
-            </Menu>
-            <RichTextExample/>
+            <Sticky>
+                <Menu borderless attached inverted>
+                    <Container>
+                        <Menu.Item>TODO：标志</Menu.Item>
+                        <Menu.Item position='left'>
+                            <Input className='icon' icon='search' placeholder='TODO'/>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Button>Log in</Button>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Button>Sign up</Button>
+                        </Menu.Item>
+                    </Container>
+                </Menu>
+            </Sticky>
+
+            <Container>
+                <Segment>
+                    <RichTextExample/>
+                </Segment>
+            </Container>
         </div>
     );
 }
