ace1b23473d01eb5f920c9a8a0c4275b24d6895e
taotsi <taotsi@protonmail.com>
2022-09-12 17:33:01 +0800

    grids for repo main page

--- src/components/repo/RepoMain.tsx
+++ src/components/repo/RepoMain.tsx
@@ -1,5 +1,5 @@
 import {Navigate, Outlet, useParams} from "react-router-dom"
-import {Container} from "semantic-ui-react"
+import {Divider, Grid} from "semantic-ui-react"
 import {queryRepository} from "../../api/BackendClient"
 import TheEditor from "../editor/TheEditor"
 
@@ -13,9 +13,25 @@ export default function RepoMain() {
 
     return (
         <div>
-            <Container text>
-                <TheEditor readOnly={true} text={repo.text}></TheEditor>
-            </Container>
+            <Grid centered>
+                <Grid.Column width={2}>
+                    <h4>About</h4>
+                    关于这个项目，这篇文章，都有些什么想说的，就写在这里
+                    <Divider/>
+                    <h4>发表</h4>
+                    当文章写的差不多了，可能就想发表。这里会列出你历史上发表的版本
+                    <Divider/>
+                    <h4>贡献者</h4>
+                    对这篇文章有贡献的人在这里列出来
+                </Grid.Column>
+                <Grid.Column width={9}>
+                    <TheEditor readOnly={true} text={repo.text}></TheEditor>
+                </Grid.Column>
+                <Grid.Column width={4}>
+                    <h4>最近评论</h4>
+                    最近的质疑和合并请求可能会放在这里，但也不一定。可能会把这个功能合并进编辑器里。可能会加一个评论功能，但是想想似乎也不是特别有必要
+                </Grid.Column>
+            </Grid>
             <Outlet/>
         </div>
     )
