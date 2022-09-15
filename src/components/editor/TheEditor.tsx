109bad86d8729bd34a343b5e1a2a5a53c2aabc6a
taotsi <taotsi@protonmail.com>
2022-09-16 02:29:09 +0800

    split code of TheEditor since it's getting bigger

--- src/components/editor/TheEditor.tsx
+++ src/components/editor/TheEditor.tsx
@@ -1,14 +1,13 @@
 import React, {useCallback, useMemo} from "react"
-import {Slate, withReact} from "slate-react"
+import {withReact} from "slate-react"
 import {createEditor} from "slate"
 import {withHistory} from "slate-history"
 import "./styles.css"
 import {EDITOR_MODE, EMPTY_TEXT} from "./constants"
-import FixedToolbar from "./FixedToolbar"
 import {TheEditorProps} from "./types"
-import {EditingArea} from "./EditingArea"
-import HoveringToolbar from "./HoveringToolBar"
-import {Grid, Segment} from "semantic-ui-react"
+import EditorEdit from "./EditorEdit"
+import EditorRead from "./EditorRead"
+import EditorDiff from "./EditorDiff"
 
 export default function TheEditor(props: TheEditorProps) {
     // @ts-ignore
@@ -21,60 +20,34 @@ export default function TheEditor(props: TheEditorProps) {
     const value = props.value === undefined ? EMPTY_TEXT : props.value
     const mode = props.mode === undefined ? EDITOR_MODE.EDIT : props.mode
 
-    let rendered = <span>wrong mode of the editor</span>
     switch (mode) {
         case EDITOR_MODE.EDIT:
-            rendered = <Slate editor={editor} value={value}>
-                <FixedToolbar mode={mode}/>
-                <EditingArea
-                    // @ts-ignore
+            return (
+                <EditorEdit
+                    editor={editor}
+                    value={value}
+                    mode={mode}
                     renderElement={renderElement}
                     renderLeaf={renderLeaf}
-                    mode={mode}
                 />
-            </Slate>
-            break
+            )
         case EDITOR_MODE.READ:
-            rendered = <Slate editor={editor} value={value}>
-                <HoveringToolbar/>
-                <Grid>
-                    <Grid.Row>
-                        <Grid.Column width={9}>
-                            <div>
-                                <FixedToolbar mode={mode}/>
-                                <EditingArea
-                                    // @ts-ignore
-                                    renderElement={renderElement}
-                                    renderLeaf={renderLeaf}
-                                    mode={mode}
-                                />
-                            </div>
-                        </Grid.Column>
-                        <Grid.Column width={4}>
-                            <IssueCard type="同义反复" notes="同义反复"/>
-                            <IssueCard type="稻草人" notes="树立不存在的攻击对象"/>
-                            <IssueCard type="缺少数据" notes="经验归纳需要提供数据证明，或者引用他人数据"/>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-
-            </Slate>
-            break
+            return (
+                <EditorRead
+                    editor={editor}
+                    value={value}
+                    mode={mode}
+                    renderElement={renderElement}
+                    renderLeaf={renderLeaf}
+                />
+            )
         case EDITOR_MODE.DIFF:
-            rendered = <span>diff mode is not implemented yet</span>
-            break
+            return (
+                <EditorDiff/>
+            )
+        default:
+            return <p>{"wrong mode of the editor, mode: " + mode}</p>
     }
-
-    return rendered
-}
-
-const IssueCard = ({type, notes}: any) => {
-    return (
-        <Segment>
-            <h5>{type}</h5>
-            <p>{notes}</p>
-        </Segment>
-    )
 }
 
 const Element = ({attributes, children, element}: any) => {
